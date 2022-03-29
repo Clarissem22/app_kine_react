5460f680fe1e1969eeac755790dc4943bd40d28a
step31 <cmille749@gmail.com>
2022-03-29 15:12:03 +0200

    home

--- src/components/comp_pages/_Home/Hello.jsx
+++ src/components/comp_pages/_Home/Hello.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
+
+const date = new Date()
 const Hello = () => {
     return (
         <div>
             <img src="img/logo.png" alt="" />
             <h1>Hello, Xava</h1>
-            <p>Nous sommes le "date", il reste "nb exo" à faire aujourd'hui</p>
+            <p>Nous sommes le {date.toLocaleDateString()} , il reste "nb exo" à faire aujourd'hui</p>
         </div>
     );
 };
