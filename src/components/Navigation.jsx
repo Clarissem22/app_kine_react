1ee7622505832a81f34a6e907d795a33c01d36b1
step31 <cmille749@gmail.com>
2022-03-29 15:55:16 +0200

    sass

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,22 +1,25 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import "../style/components/navigation.scss";
 
 const Navigation = () => {
     return (
-        <div>
-            <ul>
+        <nav>
+            <div className="logo">
             <NavLink to="/"> <li><img src="img/logo.png" alt="" /></li> </NavLink> 
+            </div>
+        <div className="navbar">
+            <ul>
             <NavLink to="/accueil"> <li><img src="img/home.png" alt="Accueil" /></li> </NavLink> 
             <NavLink to="/programme"> <li><img src="img/calendar.png" alt="Programme" /></li> </NavLink> 
             <NavLink to="/exercices"> <li><img src="img/bibliothèque.png" alt="Exercices" /></li> </NavLink> 
-            <NavLink to="/profil"> <li><img src="/img/patients.png" alt="Profil" /></li> </NavLink> 
+            <NavLink to="/profil"> <li><img src="img/patients.png" alt="Profil" /></li> </NavLink> 
             <NavLink to="/contact"> <li><img src="img/mail.png" alt="Contact" /></li> </NavLink> 
             <NavLink to="/parametres"> <li><img src="img/settings.png" alt="Parametres" /></li> </NavLink> 
             <NavLink to="/login"> <li><img src="img/logout.png" alt="Logout" /></li> </NavLink> 
-                
-               
             </ul>
         </div>
+        </nav>
     );
 };
 
